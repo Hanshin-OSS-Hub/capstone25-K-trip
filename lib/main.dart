6bc5e6c24983447f925f7305a9cd87f7364d3792
hyemin-Jo <jhmjhm1237@gmail.com>
2026-01-12 13:22:26 +0900

    2026-01-12 확인용 저장

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 // 이 파일은 K-Trip 앱의 진입점입니다.
 // MaterialApp을 설정하고, 상단 카테고리 메뉴를 탭 네비게이션으로 사용하는 메인 화면을 표시합니다.
 // 4개의 카테고리(AI 일정 추천, 지도, 리뷰, 게시판)를 관리하는 StatefulWidget입니다.
+// 0206-01-12 확인용
+
 
 import 'package:flutter/material.dart';
 import 'screens/home_ai_schedule_page.dart';
